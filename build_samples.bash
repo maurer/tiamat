9beb2d7cb7330f07f808e58e30228056a0340fa7
Matthew Maurer <matthew.r.maurer@gmail.com>
2017-08-09 01:46:41 -0400

    Add build script for samples

---
+++ build_samples.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# "bindnow" prevents most PLT generation, and -O2 (implied by some other hardenings)
+# can optimize out code in my simplistic examples
+export hardeningDisable="all";
+
+make -j9 -C samples/Juliet/testcases/CWE416_Use_After_Free/
